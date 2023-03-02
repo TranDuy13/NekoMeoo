d8721b31cc454575f1b3310e10c21bdce263724a
Duy Tran <chiba@Duys-MacBook-Air.local>
2023-03-02 21:24:53 +0700

    deloyng

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 import { Fragment } from "react";
 import { publicRoutes } from "./routes";
 import { DefaultLayout } from "./layouts";
